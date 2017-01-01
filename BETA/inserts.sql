bc331813fdd9c8c7c37d2bbe3d0dd67af7690ac0
selenkozanoglu <hande.kozanoglu@isik.edu.tr>
2017-01-01 19:28:28 +0300

    some DB insert table queries

---
+++ BETA/inserts.sql
@@ -0,0 +1,30 @@
+insert into USERS(user_type,password ,u_email,firstName,lastName,username,user_address,user_city,user_place,usrtel)
+values ('1','123456','kozanogluselen@gmail.com','Selen','Kozanoglu','Selo','Sirmakes Sok. omer amca apt. 15/7','Istanbul','Bostanci','05553509336');
+
+insert into USERS(user_type,password ,u_email,firstName,lastName,username,user_address,usrtel)
+values ('3','1234','aysenazaydemir@hotmail.com','Aysenaz','Aydemir','Aysenaz','Bla sokak blala caddes 13/5','Istanbul','Erenkoy','055334545');
+
+
+insert into USERS(user_type,password ,u_email,firstName,lastName,username,user_address,usrtel)
+values ('2','1234','nazliakdag@hotmail.com','Nazli','Akdag','Nazli','Izmit caddesi izmit sok 12/6','Kocaeli','Izmit','053221445');
+
+
+insert into USERS(user_type,password ,u_email,firstName,lastName,username,user_address,usrtel)
+values ('3','1234','gamzesarac@hotmail.com','Gamze','Sarac','Gamze','Bizim apartman bizim cadde 11/3','Istanbul','Cekmekoy','05645476');
+
+
+insert into USERS(user_type,password ,u_email,firstName,lastName,username,user_address,usrtel)
+values ('3','1234','busraefe@hotmail.com','Busra','Efe','Busra','Vali caddesi cikmaz sok.','Istanbul','Cengelkoy','05677859');
+
+
+
+insert into restaurants (rid,rest_address,rest_name,owner_id,rest_info,status)
+values ('Caddebostan','Polonez',10,'We are in food industry for 10 years',0)
+
+
+insert into restaurants (rid,rest_address,rest_name,owner_id,rest_info,status)
+values ('Kadikoy','Bay Doner',11,'We have various type of foods',0)
+
+
+insert into restaurants (rid,rest_address,rest_name,owner_id,rest_info,status)
+values ('Ortakoy','Lezzetli Kumpir',11,'Enjoy while you are eating',1)
