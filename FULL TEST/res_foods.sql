7e2c2e58d3a73ff3eb5bf39ba765029b065ce316
selenkozanoglu <hande.kozanoglu@isik.edu.tr>
2017-01-12 16:02:55 +0200

    Add files via upload

---
+++ FULL TEST/res_foods.sql
@@ -0,0 +1,45 @@
+/*
+-- Query: select * from rest_foods
+LIMIT 0, 1000
+
+-- Date: 2017-01-10 15:21
+*/
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Arabiatta','Makarna','20',40);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Pesto Soslu Penne','Makarna','25',41);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Barbeku Soslu Tavuk','Et&Tavuk','30',42);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Akdeniz Pizza','Pizza','35',43);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Tantuni Pizza','Pizza','32',44);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Sezar Salata','Salata','27',45);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (2,'Brownie Cheessecake','Tatli','17',46);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Cafe de Paris Soslu Et','Et&Tavuk','45',47);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Fajita','Et&Tavuk','32',48);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Patates Tava','Baslangic','12',49);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Sosis Tava','Baslangic','13',50);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Kori Soslu Tavuk','Et&Tavuk','22',51);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Et Burger','Burger','30',56);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Tavuk Durum','Durum','15',59);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Cheese Burger','Burger','20',61);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Vegan Burger','Burger','16',62);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Big Burger','Burger','24',63);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Mega Burger','Burger','25',64);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Elma Dilim Patates','Baslangic','7',65);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Nugget 6 li','Baslangic','8',66);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Coca Cola','Icecek','4',69);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (3,'Fanta','Icecek','4',70);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Et Durum','Durum','12',71);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (5,'Big King Menu','Burger','20',72);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (112,'Tavuk Doner','Et&Durum','10',73);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Manti','Manti','20',74);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'Dragon Roll','Sushi','24',75);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'Crunchy Roll (6 Adet)','Sushi','32',76);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,' Edamame ','Aperatifler','15',77);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'Crispy Roll (6 Adet)','Sushi','32',78);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,' California Roll (6 Adet) ','Sushi','30',79);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'Miso Soup','Corba','14',80);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,' Lemongrass Soup ','Soup','16',81);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'AvocadoTofu Salad','Salata','29',82);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Su','Icecek','3',83);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Limonata','Icecek','7',84);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Ayran','Icecek','4',85);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (10,'Sake Roll (8li)','Sushi','25',86);
+INSERT INTO `rest_foods` (`restaurant_id`,`food_name`,`food_type`,`price`,`food_id`) VALUES (4,'Iskender','Kebap','20',87);
