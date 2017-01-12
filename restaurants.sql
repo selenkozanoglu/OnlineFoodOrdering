4e0042a446f8554939cf44e97a8937d125ea373c
selenkozanoglu <hande.kozanoglu@isik.edu.tr>
2017-01-12 16:03:22 +0200

    Add files via upload

---
+++ restaurants.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * from restaurants r
+LIMIT 0, 1000
+
+-- Date: 2017-01-10 17:26
+*/
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (2,'Bebek','Sang Food House',10,NULL,'1',NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (4,'Uskudar','Trend 216',11,'Hizmette sahaneyiz','1','0216 366 49 58');
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (5,'Bostanci','As Iskender',12,NULL,NULL,NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (8,'Sile','Kokoz',15,NULL,NULL,NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (11,'eminonu','canak',NULL,'emionundsf','2',NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (12,'Bostanci','Sampiyon Kokorec',NULL,'Sampiyon','2',NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (13,'cengelkoy','sutis',NULL,'ghg','2',NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (14,'Caddebostan','Burger King',46,'Burger King sube','0',NULL);
+INSERT INTO `restaurants` (`rid`,`rest_address`,`rest_name`,`owner_id`,`rest_info`,`status`,`rest_phone`) VALUES (112,'Bebek','Polonez',10,'blalallalalal','2','0212 576857');
